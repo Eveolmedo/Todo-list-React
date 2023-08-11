817294234a8652c35cc85760d1671c487a9959b5
Evelyn Olmedo <evelynolmeedo@gmail.com>
2023-08-11 12:17:46 -0300

    Add media queries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header () {
     return (
         <header className="flex justify-center p-4">
-            <h1 className="text-[#0D160B] text-6xl font-bold">Todo List</h1>
+            <h1 className="text-[#0D160B] text-6xl font-bold mb-5">Todo List</h1>
         </header>
     )
 } 
