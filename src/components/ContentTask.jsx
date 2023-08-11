92ed4d64d99c47b9382b9b4e615cd97f708dbd37
Evelyn Olmedo <evelynolmeedo@gmail.com>
2023-08-11 10:32:42 -0300

    Add validation and filters

--- src/components/ContentTask.jsx
+++ src/components/ContentTask.jsx
@@ -1,11 +1,12 @@
 import Task from "./Task";
-const ContentTask = ({ tasks, setTasks }) => {
+const ContentTask = ({ tasks, setTasks, checkTask }) => {
     return (
         <ul className="flex flex-col items-center">
             {tasks.map((task) => {
                 return <Task
                     tasks={tasks}
                     setTasks={setTasks}
+                    checkTask={checkTask}
                     task={task}
                     completed={task.completed}
                     key={task.id}
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,18 +2,14 @@ import React, { useState } from "react";
 import { FaCheck } from "react-icons/fa";
 import { FaTrash } from "react-icons/fa";
 
-const Task = ({ task:{ task, id, completed }, tasks, setTasks}) => {
+const Task = ({ task:{ task, id, completed }, tasks, setTasks, checkTask}) => {
+
+    const filterTask = () => {
+        checkTask(id)
+    }
     
     const deleteTask = () => {
         const updatedTasks = tasks.filter((task) => task.id !== id)
-        localStorage.setItem("task", JSON.stringify(updatedTasks))
-        setTasks(updatedTasks)
-    }
-
-    const checkTask = () => {
-        const updatedTasks = tasks.map(task =>
-            task.id === id ? { ...task, completed: !task.completed } : task
-        )
         localStorage.setItem("task", JSON.stringify(updatedTasks))
         setTasks(updatedTasks)
     }
@@ -21,7 +17,7 @@ const Task = ({ task:{ task, id, completed }, tasks, setTasks}) => {
     return (
         <li className="flex justify-around aling-center w-1/2 bg-white mb-2">
             <p className={`text-3xl flex-1 p-2 ${completed ? "line-through text-gray-500" : ""}`}>{task} </p>
-            <button className={`p-5 text-xl text-white ${completed ? "bg-gray-600": "bg-[#ADF1D2]"}`} onClick={checkTask}><FaCheck /></button>
+            <button className={`p-5 text-xl ${completed ? "bg-gray-600 text-white": "bg-[#ADF1D2]"}`} onClick={filterTask}><FaCheck /></button>
             <button className="p-5 text-xl text-white bg-[#69585F]" onClick={deleteTask}><FaTrash /></button>
         </li>
     )
